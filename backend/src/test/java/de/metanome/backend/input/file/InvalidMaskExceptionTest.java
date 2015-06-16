4598bff064e32887ae83343db26076624129d26f
Jakob Zwiener <jakob.zwiener@student.hpi.uni-potsdam.de>
2015-06-16 15:43:40 +0200

    added InvalidMaskException.java

---
+++ backend/src/test/java/de/metanome/backend/input/file/InvalidMaskExceptionTest.java
@@ -0,0 +1,83 @@
+/*
+ * Copyright 2015 by the Metanome project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package de.metanome.backend.input.file;
+
+import org.junit.Test;
+
+import java.io.FileNotFoundException;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Test for {@link InvalidMaskException}.
+ *
+ * @author Jakob Zwiener
+ */
+public class InvalidMaskExceptionTest {
+
+  /**
+   * Test for {@link InvalidMaskException#InvalidMaskException(String)}
+   *
+   * The exception should store the message.
+   */
+  @Test
+  public void testInvalidMaskExceptionString() {
+    // Setup
+    // Expected values
+    String expectedMessage = "some message";
+
+    // Execute functionality
+    String actualMessage;
+    try {
+      throw new InvalidMaskException(expectedMessage);
+    } catch (InvalidMaskException e) {
+      actualMessage = e.getMessage();
+    }
+
+    // Check result
+    assertEquals(expectedMessage, actualMessage);
+  }
+
+  /**
+   * Test method for {@link InvalidMaskException#InvalidMaskException(String, Throwable)}
+   *
+   * The exceptioin should store the message and the cause.
+   */
+  @Test
+  public void testInvalidMaskExceptionStringThrowable() {
+    // Setup
+    // Expected values
+    String expectedMessage = "some message";
+    Throwable expectedCause = new FileNotFoundException("some file was not found");
+
+    // Execute functionality
+    String actualMessage;
+    Throwable actualCause;
+    try {
+      throw new InvalidMaskException(expectedMessage, expectedCause);
+    } catch (InvalidMaskException e) {
+      actualMessage = e.getMessage();
+      actualCause = e.getCause();
+    }
+
+    // Check result
+    assertEquals(expectedMessage, actualMessage);
+    assertEquals(expectedCause, actualCause);
+  }
+
+
+}
